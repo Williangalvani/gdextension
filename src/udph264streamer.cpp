5d65caa91a2fe6f601d9cfe64eaa12b06ad4faa4
Willian Galvani <williangalvani@gmail.com>
2024-01-10 23:47:58 -0300

    pure rtsp

--- src/udph264streamer.cpp
+++ src/udph264streamer.cpp
@@ -5,29 +5,45 @@
 
 using namespace godot;
 
-bool need_frame = true;
-
 #define DEFAULT_RTSP_PORT "8554"
 
-static char *port = (char *) DEFAULT_RTSP_PORT;
+static char *port = (char *)DEFAULT_RTSP_PORT;
+bool need_frame = true;
+int input_width = 1152;
+int input_height = 648;
 
 
-std::string find_working_hw_encoder() {
-    std::vector<std::string> encoders = {"nvh264enc", "msdkh264enc","vtenc_h264_hw", "x264enc"};
+std::string x264enc_factory = "( appsrc name=mysrc is-live=true ! videoconvert ! x264enc tune=zerolatency bitrate=10000 ! video/x-h264,profile=baseline ! rtph264pay name=pay0 pt=96 )";
 
-    for (const auto& encoder_name : encoders) {
+
+static void
+need_data()
+{
+    need_frame = true;
+}
+
+GstElement *appsrc = NULL;
+
+std::string find_working_hw_encoder()
+{
+    std::vector<std::string> encoders = {"nvh264enc", "msdkh264enc", "vtenc_h264_hw", "x264enc"};
+
+    for (const auto &encoder_name : encoders)
+    {
         std::string pipeline_str = "videotestsrc ! " + encoder_name + " ! fakesink";
         GError *error = NULL;
         GstElement *pipeline = gst_parse_launch(pipeline_str.c_str(), &error);
 
-        if (pipeline == NULL || error != NULL) {
+        if (pipeline == NULL || error != NULL)
+        {
             g_printerr("Failed to create pipeline: %s\n", error->message);
             g_error_free(error);
             continue;
         }
 
         GstStateChangeReturn ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
-        if (ret == GST_STATE_CHANGE_FAILURE) {
+        if (ret == GST_STATE_CHANGE_FAILURE)
+        {
             gst_object_unref(pipeline);
             continue;
         }
@@ -38,12 +54,46 @@ std::string find_working_hw_encoder() {
     }
     return "";
 }
-void UdpH264Streamer::setup_rtsp_server() {
-     GError *error = NULL;
+
+/* called when a new media pipeline is constructed. We can query the
+ * pipeline and configure our appsrc */
+static void
+media_configure(GstRTSPMediaFactory *factory, GstRTSPMedia *media,
+                gpointer user_data)
+{
+    GstElement *element;
+    GstFlowReturn ret;
+
+    /* get the element used for providing the streams of the media */
+    element = gst_rtsp_media_get_element(media);
+
+    /* get our appsrc, we named it 'mysrc' with the name property */
+    appsrc = gst_bin_get_by_name_recurse_up(GST_BIN(element), "mysrc");
+
+    /* this instructs appsrc that we will be dealing with timed buffer */
+    gst_util_set_object_arg(G_OBJECT(appsrc), "format", "time");
+    /* configure the caps of the video */
+    g_object_set(G_OBJECT(appsrc), "caps",
+                 gst_caps_new_simple("video/x-raw",
+                                     "format", G_TYPE_STRING, "RGB",
+                                     "width", G_TYPE_INT, input_width,
+                                     "height", G_TYPE_INT, input_height,
+                                     "framerate", GST_TYPE_FRACTION, 60, 1, NULL),
+                 NULL);
+
+    g_signal_connect(appsrc, "need-data", (GCallback)need_data, &ret);
+    // gst_object_unref (appsrc);
+    gst_object_unref(element);
+}
+
+void UdpH264Streamer::setup_rtsp_server()
+{
+    GError *error = NULL;
 
     // Create the RTSP server
     rtsp_server = gst_rtsp_server_new();
-    if (!rtsp_server) {
+    if (!rtsp_server)
+    {
         g_printerr("Failed to create RTSP server\n");
         return;
     }
@@ -51,258 +101,145 @@ void UdpH264Streamer::setup_rtsp_server() {
     // Set the server's service port
     g_object_set(rtsp_server, "service", port, NULL);
 
-    // Get mount points from the server
+    /* get the mount points for this server, every server has a default object
+     * that be used to map uri mount points to media factories */
     mounts = gst_rtsp_server_get_mount_points(rtsp_server);
-    if (!mounts) {
-        g_printerr("Failed to get mount points from RTSP server\n");
-        g_object_unref(rtsp_server);
-        return;
-    }
 
-    // Create a media factory
     factory = gst_rtsp_media_factory_new();
-    if (!factory) {
-        g_printerr("Failed to create RTSP media factory\n");
-        g_object_unref(mounts);
-        g_object_unref(rtsp_server);
-        return;
-    }
 
+    gst_rtsp_media_factory_set_launch(factory,
+                                      x264enc_factory.c_str());
+
+    /* notify when our media is ready, This is called whenever someone asks for
+     * the media and a new pipeline with our appsrc is created */
+    g_signal_connect(factory, "media-configure", (GCallback)media_configure,
+                     NULL);
     gst_rtsp_media_factory_set_shared(factory, TRUE);
 
-    // Launch string for the RTSP server pipeline
-    std::string launch_string =
-        "udpsrc port=" + std::to_string(udp_port) + " caps=\"application/x-rtp,media=video,clock-rate=90000,encoding-name=H264\" ! " +
-        "rtph264depay ! h264parse ! video/x-h264, width=(int)" + std::to_string(this->input_width) +
-        ", height=(int)" + std::to_string(this->input_height) + ", framerate=(fraction)60/1 ! "
-        "rtph264pay config-interval=10 name=pay0 pt=96";
-
-
-    // Set the launch string for the factory
-    gst_rtsp_media_factory_set_launch(factory, launch_string.c_str());
-
-    // Add the factory to the mount points
+    /* attach the test factory to the /test url */
     gst_rtsp_mount_points_add_factory(mounts, "/test", factory);
 
-    // Unreference the mount points as they are no longer needed
+    /* don't need the ref to the mounts anymore */
     g_object_unref(mounts);
-
-    // Start the RTSP server
-    if (gst_rtsp_server_attach(rtsp_server, NULL) == 0) {
-        g_printerr("Failed to attach RTSP server\n");
-        g_object_unref(factory);
-        g_object_unref(rtsp_server);
-        return;
-    }
+    /* attach the server to the default maincontext */
+    gst_rtsp_server_attach(rtsp_server, NULL);
 
     // Server is running now
     g_print("RTSP server is running\n");
 }
 
-UdpH264Streamer::UdpH264Streamer() {
+UdpH264Streamer::UdpH264Streamer()
+{
     gst_init(NULL, NULL);
-    main_loop = g_main_loop_new(NULL, FALSE);  // Create a new GMainLoop
-    need_frame = true;
+    main_loop = g_main_loop_new(NULL, FALSE); // Create a new GMainLoop
+    need_frame = false;
 }
 
-UdpH264Streamer::~UdpH264Streamer() {
-    if (pipeline) {
-        gst_element_set_state(pipeline, GST_STATE_NULL);
-        gst_object_unref(pipeline);
-        pipeline = NULL;
-    }
-    if (rtsp_server) {
+UdpH264Streamer::~UdpH264Streamer()
+{
+
+    if (rtsp_server)
+    {
         g_object_unref(rtsp_server);
         rtsp_server = NULL;
     }
-    if (factory) {
+    if (factory)
+    {
         g_object_unref(factory);
         factory = NULL;
     }
-    if (main_loop) {
+    if (main_loop)
+    {
         g_main_loop_unref(main_loop);
         main_loop = NULL;
     }
 }
 
-void UdpH264Streamer::_process(double delta) {
-      if (main_loop) {
+void UdpH264Streamer::_process(double delta)
+{
+    if (main_loop)
+    {
         // Iterate the GStreamer main loop
         g_info("Running main loop");
-        while (g_main_context_pending(g_main_loop_get_context(main_loop))) {
-          g_main_context_iteration(g_main_loop_get_context(main_loop), FALSE);
+        while (g_main_context_pending(g_main_loop_get_context(main_loop)))
+        {
+            g_main_context_iteration(g_main_loop_get_context(main_loop), FALSE);
         }
     }
 }
 
-
-static void
-need_data () {
-  need_frame = true;
+static void push_frame(const PackedByteArray &raw_data)
+{
+    GST_ERROR("PUSHIN FRAME");
+    if (!appsrc)
+    {
+        GST_ERROR(" APPSRC IS NULL!");
+    }
+    static GstClockTime timestamp = 0;
+    guint size;
+    GstBuffer *buffer;
+    GstFlowReturn ret;
+    buffer = gst_buffer_new_allocate(NULL, raw_data.size(), NULL);
+    gst_buffer_fill(buffer, 0, raw_data.ptr(), raw_data.size());
+    GST_BUFFER_PTS(buffer) = timestamp;
+    GST_BUFFER_DURATION(buffer) = gst_util_uint64_scale_int(1, GST_SECOND, 60);
+    timestamp += GST_BUFFER_DURATION(buffer);
+    g_signal_emit_by_name(appsrc, "push-buffer", buffer, &ret);
+    gst_buffer_unref(buffer);
 }
 
-static void push_frame(GstElement* appsrc, const PackedByteArray &raw_data) {
-  static GstClockTime timestamp = 0;
-  guint size;
-  GstBuffer *buffer;
-  GstFlowReturn ret;
-  buffer = gst_buffer_new_allocate (NULL, raw_data.size(), NULL);
-  gst_buffer_fill(buffer, 0, raw_data.ptr(), raw_data.size());
-  GST_BUFFER_PTS (buffer) = timestamp;
-  GST_BUFFER_DURATION (buffer) = gst_util_uint64_scale_int (1, GST_SECOND, 100);
-  timestamp += GST_BUFFER_DURATION (buffer);
-  g_signal_emit_by_name (appsrc, "push-buffer", buffer, &ret);
-  gst_buffer_unref (buffer);
-}
-
-void UdpH264Streamer::setup_gstreamer_pipeline() {
-    g_printerr ("creating pipeline...\n");
-    pipeline = gst_pipeline_new("mypipeline");
-    appsrc = gst_element_factory_make("appsrc", "mysrc");
-    videoconvert = gst_element_factory_make("videoconvert", "myvideoconvert");
-    capsfilter = gst_element_factory_make("capsfilter", "mycapsfilter");
-    udpsink = gst_element_factory_make("udpsink", "myudpsink");
-    queue1 = gst_element_factory_make("queue", "myqueue1");
-    queue2 = gst_element_factory_make("queue", "myqueue2");
-    h264parse = gst_element_factory_make("h264parse", "myh264parser");
-    rtph264pay = gst_element_factory_make("rtph264pay", "myrtppay");
-
-    GstCaps *appsrccaps = gst_caps_new_simple("video/x-raw",
-        "format", G_TYPE_STRING, "RGB",  // Adjust as necessary
-        "width", G_TYPE_INT, input_width,
-        "height", G_TYPE_INT, input_height,
-        "framerate", GST_TYPE_FRACTION, 60, 1,  // Setting a fixed framerate
-        NULL);
-    g_object_set(G_OBJECT(appsrc), "caps", appsrccaps,"format", GST_FORMAT_TIME, "is-live", true, NULL);
-    gst_caps_unref(appsrccaps);
-
-    // Create the caps for the h264parse element
-    GstCaps *caps = gst_caps_from_string("video/x-h264,stream-format=avc,alignment=au,profile=baseline");
-    if (!caps) {
-        g_printerr("Failed to create caps.\n");
-        return;
-    }
-    g_object_set(h264parse, "config-interval", 2, NULL);
-
-    // Get the source pad of the h264parse element
-    GstPad *pad = gst_element_get_static_pad(h264parse, "src");
-    if (!pad) {
-        g_printerr("Failed to get source pad of h264parse.\n");
-        gst_caps_unref(caps);
-        return;
-    }
-
-    // Set the caps on the pad
-    if (!gst_pad_set_caps(pad, caps)) {
-        g_printerr("Failed to set caps on h264parse.\n");
-    }
-
-    // Unref the pad and the caps
-    gst_object_unref(pad);
-    gst_caps_unref(caps);
-
-
-    // Configure caps for capsfilter if needed
-GstCaps *convert_caps = gst_caps_new_simple("video/x-raw",
-    "format", G_TYPE_STRING, "I420",
-    "width", G_TYPE_INT, input_width,
-    "height", G_TYPE_INT, input_height,
-    "framerate", GST_TYPE_FRACTION, 60, 1,  // Setting a fixed framerate
-    NULL);
-    g_object_set(G_OBJECT(capsfilter), "caps", convert_caps, NULL);
-    gst_caps_unref(convert_caps);
-    g_object_set (udpsink, "host", "127.0.0.1", "port", udp_port, NULL);
-    // Check for macOS to decide on the encoder
-
-    // TODO: use this to pick the encoder
-    auto encoder = find_working_hw_encoder();
-    GST_ERROR("Encoder: %s\n", encoder.c_str());
-
-    if (encoder == "vtenc_h264_hw") {
-      x264enc = gst_element_factory_make("x264enc", "myencoder");
-      g_object_set(x264enc, "pass", 4, NULL);  // Set pass to 'quantizer'
-      g_object_set(x264enc, "quantizer", 20, NULL);  // Set quantizer to a reasonable value
-      g_object_set(x264enc, "tune", 4, NULL);  // Set tune to 'zerolatency'
-      g_object_set(x264enc, "bitrate", 5000, NULL);
-      g_object_set(x264enc, "key-int-max", 120, NULL);  // Set keyframe interval to 2 seconds
-    } else if (encoder == "x264enc") {
-      x264enc = gst_element_factory_make("x264enc", "myencoder");
-      g_object_set(x264enc, "pass", 4, NULL);  // Set pass to 'quantizer'
-      g_object_set(x264enc, "quantizer", 20, NULL);  // Set quantizer to a reasonable value
-      g_object_set(x264enc, "tune", 4, NULL);  // Set tune to 'zerolatency'
-      g_object_set(x264enc, "bitrate", 5000, NULL);
-      g_object_set(x264enc, "key-int-max", 120, NULL);  // Set keyframe interval to 2 seconds
-    } else if (encoder == "nvh264enc") {
-      x264enc = gst_element_factory_make("nvh264enc", "myencoder");
-
-      g_object_set(x264enc, "bitrate", 5000, NULL);
-      g_object_set(x264enc, "rc-mode", 2,/*(2): cbr           - CBR*/ NULL);
-      g_object_set(x264enc, "qos", TRUE, NULL);
-      g_object_set(x264enc, "preset", 4 /*low-latency-hq*/, NULL);
-      g_object_set(x264enc, "gop-size", 60, NULL);
-      GST_ERROR("UPDATED CODE2");
-
-      GstCaps *nvh264enc_caps = gst_caps_from_string("video/x-h264,stream-format=avc,alignment=au,profile=baseline");
-      nvh264enc_capsfilter = gst_element_factory_make("capsfilter", NULL);
-      g_object_set(nvh264enc_capsfilter, "caps", nvh264enc_caps, NULL);
-      gst_caps_unref(nvh264enc_caps);
-
-    } else if (encoder == "vaapih264enc") {
-      x264enc = gst_element_factory_make("vaapih264enc", "myencoder");
-
-    } else {
-      g_printerr("Invalid encoder: %s\n", encoder.c_str());
-      return;
-    }
-    g_signal_connect (appsrc,  "need-data", G_CALLBACK (need_data), NULL);
-
-    if (!pipeline || !appsrc || !videoconvert || !capsfilter || !x264enc || !h264parse || !queue1 || !queue2 || !rtph264pay || !udpsink) {
-        g_printerr("Failed to create one or more elements.\n");
-        return;
-    }
-
-    // Configure your elements as needed, e.g., set properties on appsrc, x264enc, udpsink
-
-    if (encoder == "nvenc_h264") {
-      gst_bin_add_many(GST_BIN(pipeline), appsrc, x264enc, nvh264enc_capsfilter, queue1, rtph264pay, udpsink, NULL);
-      gst_element_link_many(appsrc, x264enc, nvh264enc_capsfilter, queue1, rtph264pay, udpsink, NULL);
-    } else {
-      gst_bin_add_many(GST_BIN(pipeline), appsrc, videoconvert, capsfilter, queue1, x264enc, h264parse, queue2, rtph264pay, udpsink, NULL);
-      gst_element_link_many(appsrc, videoconvert, capsfilter, queue1, x264enc, h264parse, queue2, rtph264pay, udpsink, NULL);
-    }
-    // Start the pipeline
-    gst_element_set_state(pipeline, GST_STATE_PLAYING);
-    g_printerr ("set to playing\n");
-
-    setup_rtsp_server();
-
-}
-
-void UdpH264Streamer::push_buffer_to_gstreamer(const PackedByteArray &raw_data) {
+void UdpH264Streamer::push_buffer_to_gstreamer(const PackedByteArray &raw_data)
+{
     // Check if pipeline and essential elements are created
-    if (!pipeline || !appsrc || !videoconvert || !x264enc || !rtph264pay || !udpsink || !queue1 || !queue2) {
+    if (!rtsp_server)
+    {
         g_printerr("Pipeline or one of its elements is not created, setting up the pipeline...\n");
-        setup_gstreamer_pipeline();
+        setup_rtsp_server();
         return;
     }
 
     // If the pipeline is playing and a new frame is needed, push the frame
-    if (need_frame) {
-        push_frame(appsrc, raw_data);
-        need_frame = false;
+    if (need_frame)
+    {
+        push_frame(raw_data);
+        // need_frame = false;
+    }
+    else
+    {
+        GST_ERROR("NO NEED FRAME");
     }
 }
 
-int UdpH264Streamer::get_port() {
-    return udp_port;
+int UdpH264Streamer::get_port()
+{
+    return rtsp_port;
 }
 
-void UdpH264Streamer::set_port(int port) {
-    udp_port = port;
+void UdpH264Streamer::set_port(int port)
+{
+    rtsp_port = port;
 }
 
-void UdpH264Streamer::_bind_methods() {
-    ClassDB::bind_method(D_METHOD("setup_gstreamer_pipeline"), &UdpH264Streamer::setup_gstreamer_pipeline);
+
+int UdpH264Streamer::get_input_width() {
+    return input_width;
+}
+
+void UdpH264Streamer::set_input_width(int width) {
+    input_width = width;
+}
+
+int UdpH264Streamer::get_input_height() {
+    return input_height;
+}
+
+void UdpH264Streamer::set_input_height(int height) {
+    input_height = height; 
+}
+
+
+void UdpH264Streamer::_bind_methods()
+{
     ClassDB::bind_method(D_METHOD("push_buffer_to_gstreamer"), &UdpH264Streamer::push_buffer_to_gstreamer);
 
     ClassDB::bind_method(D_METHOD("get_input_width"), &UdpH264Streamer::get_input_width);
@@ -314,6 +251,6 @@ void UdpH264Streamer::_bind_methods() {
     ClassDB::add_property("UdpH264Streamer", PropertyInfo(Variant::INT, "input_height"), "set_input_height", "get_input_height");
 
     ClassDB::bind_method(D_METHOD("get_port"), &UdpH264Streamer::get_port);
-    ClassDB::bind_method(D_METHOD("set_port", "udp_port"), &UdpH264Streamer::set_port);
-    ClassDB::add_property("UdpH264Streamer", PropertyInfo(Variant::INT, "udp_port"), "set_port", "get_port");
+    ClassDB::bind_method(D_METHOD("set_port", "rtsp_port"), &UdpH264Streamer::set_port);
+    ClassDB::add_property("UdpH264Streamer", PropertyInfo(Variant::INT, "rtsp_port"), "set_port", "get_port");
 }
--- src/udph264streamer.h
+++ src/udph264streamer.h
@@ -10,18 +10,18 @@
 
 namespace godot {
 
+
 class UdpH264Streamer : public Node {
     GDCLASS(UdpH264Streamer, Node)
 
 private:
     GstElement *nvh264enc_capsfilter = NULL, *appsrc = NULL, *pipeline = NULL, *videoconvert = NULL, *x264enc = NULL, *rtph264pay = NULL, *udpsink = NULL, *queue1 = NULL, *queue2 = NULL, *capsfilter = NULL, *h264parse = NULL;
-    int udp_port = 5600;
+    int rtsp_port = 5600;
     GstRTSPServer *rtsp_server = NULL;
     GstRTSPMountPoints *mounts = NULL;
     GstRTSPMediaFactory *factory = NULL;
     GMainLoop *main_loop = NULL;
-    int input_width = 1152;
-    int input_height = 648;
+
 
 protected:
     static void _bind_methods();
@@ -33,12 +33,11 @@ public:
     int get_port();
     void set_port(int port);
     // input_size
-    int get_input_width() { return input_width; };
-    void set_input_width(int width) { input_width = width; };
-    int get_input_height() { return input_height; };
-    void set_input_height(int height) { input_height = height; };
+    int get_input_width();
+    void set_input_width(int width);
+    int get_input_height();
+    void set_input_height(int height);
 
-    void setup_gstreamer_pipeline();
     void _process(double delta) override;
     void push_buffer_to_gstreamer(const PackedByteArray &raw_data);
     void setup_rtsp_server();
